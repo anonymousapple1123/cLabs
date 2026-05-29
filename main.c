da7ce155435a0cfc54f3c8d1309ca73f206e6a28
owner <daemonscarte1123@gmail.com>
2026-05-29 21:48:08 +0530

    added socket

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-
+#include <sys/socket.h>
 const int TRUE = 1;
  int main(){
+    int fn = socket(AF_INET, SOCK_STREAM , 0);
+
      while(TRUE){
          printf("Waitiing for inputtt \n");
          sleep(1);
